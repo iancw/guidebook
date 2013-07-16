b1409a60b5ab5605506d0ac7ec1056cb9d919f60
Ian Will <icwill@gmail.com>
2013-07-15 20:38:07 -0400

    Created initial classes

---
+++ SenecaGuidebook/SenecaQuartzView.h
@@ -0,0 +1,13 @@
+//
+//  SenecaQuartzView.h
+//  SenecaGuidebook
+//
+//  Created by Ian Will on 7/14/13.
+//  Copyright (c) 2013 Ian Will Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SenecaQuartzView : QuartzView
+
+@end
---
+++ SenecaGuidebook/SenecaQuartzView.m
@@ -0,0 +1,13 @@
+//
+//  SenecaQuartzView.m
+//  SenecaGuidebook
+//
+//  Created by Ian Will on 7/14/13.
+//  Copyright (c) 2013 Ian Will Software. All rights reserved.
+//
+
+#import "SenecaQuartzView.h"
+
+@implementation SenecaQuartzView
+
+@end
